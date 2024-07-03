fd2ae02f09363adc3a2986537b6d5ad74ee9d383
Taha Rehman <rehmantaha77@gmail.com>
2024-07-03 22:15:36 +0500

    completed ToDO List

--- ToDoList/script.js
+++ ToDoList/script.js
@@ -1,6 +1,5 @@
 const popWindow = document.getElementById('addWindow');
 const nameInput = document.getElementById('nameInput');
-const cursor = document.cursor;
 const ul = document.getElementById('taskList');
 let x = 0;
 //opening the task window
@@ -23,29 +22,33 @@ function taskAdd(){
     li.appendChild(taskButton);
     ul.appendChild(li);
     x++;
+    scrollBar();
     closePop();
     }
     
 }
 
-
 // making buttons on tasks
 function taskButtons() {
     const taskButtonsContainer = document.createElement('div');
     const doneButton = document.createElement('button');
-    const desButton = document.createElement('button');
-    desButton.className = "desButton";
     doneButton.className = 'doneButton';  
     doneButton.textContent = 'done';
-    desButton.textContent = "detail";
     taskButtonsContainer.className = 'li-buttons';
-    taskButtonsContainer.appendChild(desButton);
     taskButtonsContainer.appendChild(doneButton);
-
     doneButton.addEventListener('click', function(){
         const listElement = doneButton.parentElement.parentElement;
         ul.removeChild(listElement);
+        x--;
     });
 
     return taskButtonsContainer;
+}
+function scrollBar(){
+    if(x > 5){
+        ul.style.overflowY = 'scroll';
+    }
+    else{
+        ul.style.overflowY = 'hidden';
+    }
 }
