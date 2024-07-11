30833509739c1676d5f04e788734af5d424a84de
Taha Rehman <rehmantaha77@gmail.com>
2024-07-11 16:54:39 +0500

    figured out Basic functionality of quiz Program

--- QuizProgram/script.js
+++ QuizProgram/script.js
@@ -2,6 +2,12 @@
 document.addEventListener('DOMContentLoaded',()=>{
     const answer = ['1', '3', '4', '1', '4'];
     let answerIndex = 0;
+    let selected;
+    const scoreElement = document.getElementById('score');
+    let score=0;
+    const finishButton = document.getElementById('finish');
+    finishButton.style.display = 'none';
+    scoreElement.style.display = 'none';
     const questions = [
         'Select the 1st option',
         'Select the 3rd option',
@@ -11,20 +17,40 @@ document.addEventListener('DOMContentLoaded',()=>{
     ]
     const options = document.querySelectorAll('.option');
     const questionElement = document.getElementById('question');
-
-
-    //next Button functionality
-    const next = document.getElementById('nextQuestion');
-    next.addEventListener('click', ()=>{
-        
-    });
-
-
     //handling selected inputs 
-    for(let select in options){
-        select.addEventListener('click',()=>{
-            selected = options[select];
+    for(let x of options){
+        x.addEventListener('click',()=>{
+            selected = x;
+            console.log('inputCheck');
         });
         
     };
+
+
+    //finish button
+    function finishQuiz(){
+        scoreElement.textContent = score + "/"+answer.length;
+        scoreElement.style.display = 'block';
+    }
+
+    finishButton.addEventListener('click', finishQuiz);
+
+    //next button functionality
+    function nextFunction(){
+        if(selected.textContent === answer[answerIndex]){
+            score++;
+        }
+        console.log('check');
+        answerIndex++;
+        if(answerIndex<answer.length){
+            questionElement.textContent = questions[answerIndex];}
+        else{
+            next.removeEventListener('click', nextFunction);
+            next.style.display = 'none';
+            finishButton.style.display = 'block';
+        }
+    };
+    const next = document.getElementById('nextQuestion');
+    next.addEventListener('click', nextFunction);
+
 });
