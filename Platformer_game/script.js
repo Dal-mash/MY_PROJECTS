73ac58ad3d7a0660d138231b58f80ab85b9d449a
Taha Rehman <rehmantaha77@gmail.com>
2024-07-19 20:54:18 +0500

    added collision blocks

--- Platformer_game/script.js
+++ Platformer_game/script.js
@@ -17,6 +17,24 @@ let ctx = canvas.getContext('2d');
 ctx.fillStyle = "red";
 
 
+var collisionBlocks2D = [];
+//Turning collisions array into a 2d array
+for(let i=0;i<floorCollisions.length;i+=64){
+    collisionBlocks2D.push(floorCollisions.slice(i, i+64))
+}
+var collisionBlocks=[];
+console.log(collisionBlocks2D);
+collisionBlocks2D.forEach((row, y) => {
+    row.forEach((entity, x) => {
+        if(entity === 17 ){
+            collisionBlocks.push(new collisionBlock({
+                x:x*16,
+                y:y*16
+            }))
+        }        
+    });    
+});
+
 // Loading map sprite
 var backGround = new Sprite({
     position:{
@@ -31,14 +49,20 @@ let player = new Player({
     y:200
 })
 
+
+
 function updateSquare() {
     requestAnimationFrame(updateSquare);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.save();
-    ctx.scale(3,3);
-    ctx.translate(-40,-backGround.image.height+scaledCanvas.height)
+    // ctx.scale(3,3);
+    // ctx.translate(-40,-backGround.image.height+scaledCanvas.height)
     backGround.update();
+    collisionBlocks.forEach(block => {
+        block.update()        
+    });
     ctx.restore();
+
     player.update();
     
 }
